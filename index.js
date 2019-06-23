38971c727f8fa99cbd9b85b3da7536388877496c
Felix Martinez <felixaviermartinez@gmail.com>
2019-06-23 15:25:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // write your code below!
+
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return `Hapy holidays, ${name}!`
+}
+
+function happyHolidaysTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(countdown, name) {
+  return `It's ${days} days until ${holiday}!`
+}
+
+var name = "Felix";
+var holiday = "Christmas";
+var days = "24";
