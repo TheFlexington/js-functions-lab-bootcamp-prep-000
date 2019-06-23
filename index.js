b1d63d0f3a556af21741439b2530c50bc404ade1
Felix Martinez <felixaviermartinez@gmail.com>
2019-06-23 15:34:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function happyHolidaysTo(name) {
   return `Hapy holidays, ${name}!`
 }
 
-function happyHolidaysTo(holiday, name) {
+function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
